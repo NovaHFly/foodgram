1c0671d5b1489422e16479d69af9103c2b4bfaa6
NovaHFly <mugiviska@gmail.com>
2024-10-20 15:31:56 +0300

    fix: Make migrations and add on_delete to foreign keys

--- backend/apps/recipes/models.py
+++ backend/apps/recipes/models.py
@@ -17,6 +17,7 @@ class Ingredient(models.Model):
     name = models.CharField(
         max_length=32,
     )
+    # TODO: Rename to measurement_unit
     unit = models.CharField(
         max_length=8,
     )
@@ -30,7 +31,9 @@ class Recipe(models.Model):
         User,
         on_delete=models.CASCADE,
     )
+    # TODO: Rename cook_time to cooking_time
     cook_time = models.IntegerField()
+    # TODO: Rename description to text
     description = models.TextField()
     image = models.ImageField(
         upload_to='recipes/images/',
@@ -42,8 +45,12 @@ class RecipeIngredient(models.Model):
     recipe = models.ForeignKey(
         Recipe,
         related_name='ingredients',
+        on_delete=models.CASCADE,
     )
-    ingredient = models.ForeignKey(Ingredient)
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+    )
     amount = models.IntegerField()
 
 
