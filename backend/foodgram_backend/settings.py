1cdb9cfe78d209a797da726a44ec7604cd25f47e
NovaHFly <mugiviska@gmail.com>
2024-10-21 22:09:37 +0300

    chore: Setup token authentication

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -98,6 +98,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
