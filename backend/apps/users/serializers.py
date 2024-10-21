ba670a9147f1dbff9865d7c71f75daae3d40d26e
NovaHFly <mugiviska@gmail.com>
2024-10-21 22:10:17 +0300

    feat: Add serializers for avatars (stub) and auth

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import User
+from .models import User, UserProfile
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,6 +14,28 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name',
             'password',
         )
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+        UserProfile.objects.create(user=user)
+        return user
+
+
+class AvatarSerializer(serializers.Serializer):
+    avatar = serializers.CharField()
+
+
+class AuthSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+    def validate(self, attrs):
+        user = self.user = User.objects.get(email=attrs['email'])
+        if not user.check_password(attrs['password']):
+            raise serializers.ValidationError('Invalid password!')
+        return attrs
