d98be2aabb04721ef620a6a891c8a8f599088a9b
NovaHFly <mugiviska@gmail.com>
2024-10-21 20:53:25 +0300

    chore: Add authtoken to apps

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'djoser',
+    'rest_framework.authtoken',
     'users',
     'recipes',
 ]
