61d922f1bdea299d0a6882fb432210487ec438c9
NovaHFly <mugiviska@gmail.com>
2024-10-21 20:36:29 +0300

    chore: Remove simplejwt

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'djoser',
     'users',
     'recipes',
