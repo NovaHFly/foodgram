1c64e39c634ba6c701ca220ff06cd23350f6e681
NovaHFly <mugiviska@gmail.com>
2024-10-22 17:32:54 +0300

    feat: Register app models in admin

--- backend/apps/recipes/admin.py
+++ backend/apps/recipes/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient, Recipe, Tag
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'unit',
+    )
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'slug',
+        'name',
+    )
+    list_display_links = ('id', 'slug')
+    search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'author',
+    )
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'author')
