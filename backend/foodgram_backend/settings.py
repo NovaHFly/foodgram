f5287b2e4bd52b5a59bd3ecd2bffc5b43840dc97
NovaHFly <mugiviska@gmail.com>
2024-10-23 18:31:11 +0300

    chore: Add django-annoying

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'annoying',
     'rest_framework',
     'rest_framework.authtoken',
     'users',
