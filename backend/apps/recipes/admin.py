0f8626138346451a7fccf3311eacdc3b9529e93c
NovaHFly <mugiviska@gmail.com>
2024-10-23 19:30:11 +0300

    fix: Remove id fields from recipe admin

--- backend/apps/recipes/admin.py
+++ backend/apps/recipes/admin.py
@@ -18,44 +18,41 @@ class RecipeIngredientInline(admin.StackedInline):
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
         'name',
         'measurement_unit',
     )
-    list_display_links = ('id', 'name')
+    list_display_links = ('name',)
     search_fields = ('name',)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
         'slug',
         'name',
     )
-    list_display_links = ('id', 'slug')
+    list_display_links = ('slug',)
     search_fields = ('name',)
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
         'name',
         'author',
     )
-    list_display_links = ('id', 'name')
+    list_display_links = ('name',)
     search_fields = ('name', 'author')
     inlines = (RecipeIngredientInline,)
 
 
 @admin.register(ShortLink)
 class ShortLinkAdmin(admin.ModelAdmin):
-    list_display = ('id', 'short_url', 'full_url')
-    list_display_links = ('id', 'short_url')
+    list_display = ('short_url', 'full_url')
+    list_display_links = ('short_url',)
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user')
-    list_display_links = ('id', 'user')
+    list_display = ('user',)
+    list_display_links = ('user',)
