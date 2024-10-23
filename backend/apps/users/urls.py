0df07da23af7bd35512a91f10b5b85be1a54327a
NovaHFly <mugiviska@gmail.com>
2024-10-23 21:01:34 +0300

    fix: Remove redundant urls.py

--- backend/apps/users/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import AuthView, UsersView
-
-router = DefaultRouter()
-router.register('users', UsersView, basename='users')
-router.register('auth/token', AuthView, basename='auth')
-
-urlpatterns = router.urls
