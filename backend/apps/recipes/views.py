55ff7bb4a1a6deab129c310a97e8c266ce41e18f
NovaHFly <mugiviska@gmail.com>
2024-10-28 00:08:44 +0300

    fix: Remove unused import

--- backend/apps/recipes/views.py
+++ backend/apps/recipes/views.py
@@ -4,7 +4,6 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
