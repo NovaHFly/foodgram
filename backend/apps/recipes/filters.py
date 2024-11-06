b05e5d612998437209935c4c4efc8c61429f9229
NovaHFly <mugiviska@gmail.com>
2024-11-06 18:42:10 +0300

    fix: Remove redundant shopping cart filters

--- backend/apps/recipes/filters.py
+++ backend/apps/recipes/filters.py
@@ -1,7 +1,4 @@
 import django_filters
-from django.db.models import QuerySet
-
-from common.const import BOOLEAN_NUMBER_CHOICES
 
 from .models import Ingredient, Recipe, Tag
 
@@ -15,14 +12,6 @@ class IngredientFilter(django_filters.FilterSet):
 
 
 class RecipeFilter(django_filters.FilterSet):
-    author = django_filters.NumberFilter(
-        lookup_expr='id',
-    )
-    is_in_shopping_cart = django_filters.TypedChoiceFilter(
-        choices=BOOLEAN_NUMBER_CHOICES,
-        method='in_current_user_shopping_cart',
-        coerce=int,
-    )
     tags = django_filters.ModelMultipleChoiceFilter(
         field_name='tags__slug',
         to_field_name='slug',
@@ -31,30 +20,7 @@ class RecipeFilter(django_filters.FilterSet):
 
     class Meta:
         model = Recipe
-        fields = ['author', 'tags', 'is_in_shopping_cart']
-
-    def _check_current_user_in_lookup(
-        self,
-        lookup: str,
-        queryset: QuerySet,
-        value: int,
-    ) -> QuerySet:
-        if self.request.user.is_anonymous:
-            return queryset.none()
-
-        lookup_params = {lookup + '__in': [self.request.user]}
-        if not value:
-            return queryset.exclude(**lookup_params)
-        return queryset.filter(**lookup_params)
-
-    def in_current_user_shopping_cart(
-        self,
-        queryset: QuerySet,
-        name: str,
-        value: int,
-    ) -> QuerySet:
-        return self._check_current_user_in_lookup(
-            'shoppingcart__user',
-            queryset,
-            value,
-        )
+        fields = [
+            'author',
+            'tags',
+        ]
